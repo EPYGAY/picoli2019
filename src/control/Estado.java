c15a48dda9da7c9318b30ffbbb10a6f5049ea7e3
sergiogalan99 <sergiocalamonte2018@gmail.com>
2019-04-12 10:20:52 +0200

    Estado y Empresa modifocado

--- src/control/Estado.java
+++ src/control/Estado.java
@@ -1,5 +1,51 @@
 package control;
 
 public class Estado {
+	
+	private String nombre;
+	private int identificador,NV=365,ahorro,edad,edadMax;
+	private float salario;
+	
+	public void pasarAnno() {
+
+	}
+	
+	public void crearEmpresa() {
+
+	}
+	
+	public void destruirEmpresa() {
+
+	}
+	
+	public void contartarTrabajador() {
+
+	}
+
+	public void despedirTrabajador() {
+
+	}
+
+	public void aumentarNacimiento() {
+
+	}
+
+	public void disminuirNacimiento() {
+
+	}
+	
+	public void recolectarCapital() {
+
+	}
+	
+	public void comprobarSueldo() {
+
+	}
+	
+	public void pagarSubsidio() {
+
+	}
+
+
 
 }
