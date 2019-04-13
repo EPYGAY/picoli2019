881be93a164b5ec14fb722b18966610312af675b
dariasv1108 <49229950+dariasv1108@users.noreply.github.com>
2019-04-13 13:36:43 +0200

    jubilar

--- src/modelo/control/SeresVivos.java
+++ src/modelo/control/SeresVivos.java
@@ -2,11 +2,59 @@ package modelo.control;
 
 public class SeresVivos {
 
+	public int getAhorro() {
+		return ahorro;
+	}
+
+	public void setAhorro(int ahorro) {
+		this.ahorro = ahorro;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	public TipoSerVivo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoSerVivo tipo) {
+		this.tipo = tipo;
+	}
+
 	private String nombre;
 	private int identificador, NV = 365, ahorro, edad, edadMax;
 	private float salario;
 	private TipoSerVivo tipo;
-	
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(int identificador) {
+		this.identificador = identificador;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getEdadMax() {
+		return edadMax;
+	}
+
+	public void setEdadMax(int edadMax) {
+		this.edadMax = edadMax;
+	}
+
 	public void calcularSalario() {
 		//TODO
 	}
