82a517e142f61fab163902ea004599efcea38b4d
lespinol01 <49229885+lespinol01@users.noreply.github.com>
2019-04-22 12:41:27 +0200

    Tipos

--- src/modelo/control/TipoSerVivo.java
+++ src/modelo/control/TipoSerVivo.java
@@ -1,5 +1,22 @@
 package modelo.control;
 
 public enum TipoSerVivo {
-	menor,trabajador,desempleado,jubilado
+	menor("50"),trabajador("100"),desempleado("0"),jubilado("30");
+	
+	private String TipoSerVivo ;
+
+	private TipoSerVivo(String tipoSerVivo) {
+		TipoSerVivo = tipoSerVivo;
+	}
+
+	public String getTipoSerVivo() {
+		return TipoSerVivo;
+	}
+
+	public void setTipoSerVivo(String tipoSerVivo) {
+		TipoSerVivo = tipoSerVivo;
+	}
+	
+	
+	
 }
