b8e0b86fba016e364ebe150d6268536ae50a43ed
sergiogalan99 <sergiocalamonte2018@gmail.com>
2019-05-06 20:26:34 +0200

    Sergio.Pruebas iniciales

--- src/test/EstadoTest.java
+++ src/test/EstadoTest.java
@@ -1,26 +1,29 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import modelo.control.Empresa;
 import modelo.control.Estado;
+import utilesglobal.Constantes;
 
 class EstadoTest {
 
 	private Estado estado;
-	
+
 	@BeforeEach
 	void setUp() {
-		estado=new Estado();
-		
-		
+		estado = new Estado();
+
 	}
+
 	@Test
 	void testDespedir() {
-		//estado.condicionesIniciales();
+		// estado.condicionesIniciales();
 		estado.despedir(20);
 		assertEquals(20, estado.getNumeroDesempleados());
 		assertEquals(80, estado.getNumeroTrabajadores());
@@ -31,10 +34,23 @@ class EstadoTest {
 		assertEquals(70, estado.getNumeroDesempleados());
 		assertEquals(30, estado.getNumeroTrabajadores());
 	}
-	@Ignore
+
+	@Test
 	public void TestNacer() {
 		estado.nacer(8);
 		assertEquals(8, estado.numeroNacimientos);
+	}
+
+	@Test
+	void testCondicionesIniciales() {
+		
+		assertEquals(Constantes.NUMERO_MENORES_INICIAL, estado.getNumeroMenores());
+		assertEquals(Constantes.NUMERO_JUBILADOS_INICIAL, estado.getNumeroJubilados());
+		assertEquals(Constantes.NUMERO_TRABAJADORES_INICIAL, estado.getNumeroTrabajadores());
+		estado.condicionesIniciales();
+		assertEquals(Constantes.NUMERO_MENORES_INICIAL * 2, estado.getNumeroMenores());
+		assertEquals(Constantes.NUMERO_JUBILADOS_INICIAL * 2, estado.getNumeroJubilados());
+		assertEquals(Constantes.NUMERO_TRABAJADORES_INICIAL * 2, estado.getNumeroTrabajadores());
 	}
 
 }
